ef37bcd02774a5a0d943478effecf6f28d965009
rachel-lim <rachellim@berkeley.edu>
2019-11-03 14:20:24 -0800

    Updates

---
+++ precache-manifest.35c5cf228ccceec21ac53a033bbabdce.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "17a4c1228b767008bc5dd663f562f671",
+    "url": "/jvn-calc/index.html"
+  },
+  {
+    "revision": "416f2ae218a27c69556c",
+    "url": "/jvn-calc/static/css/main.9933f991.chunk.css"
+  },
+  {
+    "revision": "05e525fe2943a533b77e",
+    "url": "/jvn-calc/static/js/2.6ec7d264.chunk.js"
+  },
+  {
+    "revision": "416f2ae218a27c69556c",
+    "url": "/jvn-calc/static/js/main.b5f111b7.chunk.js"
+  },
+  {
+    "revision": "3c432d964fa40cd083ce",
+    "url": "/jvn-calc/static/js/runtime-main.7d0781eb.js"
+  }
+]);
